cae9936df4dfd48bb77e34b1cb26c775c74afc3a
vlymar <victorl@berkeley.edu>
2016-09-14 19:50:23 -0700

    Adds a spec

--- test/functional/pdf_helper_test.rb
+++ test/functional/pdf_helper_test.rb
@@ -8,7 +8,29 @@ module ActionController
   end
 end
 
+module ActionControllerMock
+  class Base
+    def render(_)
+      [:base]
+    end
+
+    def render_to_string
+    end
+
+    def self.after_action(_)
+    end
+  end
+end
+
 class PdfHelperTest < ActionController::TestCase
+  module SomePatch
+    def render(_)
+      super.tap do |s|
+        s << :patched
+      end
+    end
+  end
+
   def setup
     @ac = ActionController::Base.new
   end
@@ -21,5 +43,29 @@ class PdfHelperTest < ActionController::TestCase
     options = @ac.send(:prerender_header_and_footer,
                        :header => { :html => { :template => 'hf.html.erb' } })
     assert_match %r{^file:\/\/\/.*wicked_header_pdf.*\.html}, options[:header][:html][:url]
+  end
+
+  test 'should not interfere with already prepended patches' do
+    # Emulate railtie
+    if Rails::VERSION::MAJOR >= 5
+      OriginalBase = ActionController::Base
+      ActionController.send(:remove_const, :Base)
+      ActionController.const_set(:Base, ActionControllerMock::Base)
+
+      # Emulate another gem being loaded before wicked
+      ActionController::Base.prepend(SomePatch)
+      ActionController::Base.prepend(::WickedPdf::PdfHelper)
+
+      ac = ActionController::Base.new
+
+      begin
+        assert_equal ac.render(:cats), [:base, :patched]
+      rescue SystemStackError
+        assert_equal true, false # force spec failure
+      ensure
+        ActionController.send(:remove_const, :Base)
+        ActionController.const_set(:Base, OriginalBase)
+      end
+    end
   end
 end
